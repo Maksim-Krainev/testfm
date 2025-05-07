d78effda0195495873fd3c0bb72b2bc483317cdc
Vitche Research Team Developer <developer@vitche.com>
2025-05-07 21:35:22 +0300

    edit context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react'
+import { useState, useContext } from 'react'
 import { tonConnectUI } from '../tonConnectUI'
 import { WalletContext } from '../components/WalletContext'
 import { useNavigate, useLocation } from 'react-router-dom'
