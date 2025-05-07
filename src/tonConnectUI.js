582e7d246f8f55fd8d6173bf3f87d8a6feb6df91
Vitche Research Team Developer <developer@vitche.com>
2025-05-07 17:02:45 +0300

    add new conect

--- src/tonConnectUI.js
+++ src/tonConnectUI.js
@@ -1,3 +1,6 @@
+// src/tonConnectUI.js
+import { TonConnectUI } from '@tonconnect/ui-react'
+
 export const tonConnectUI = new TonConnectUI({
 	manifestUrl: 'https://fuckmetest.netlify.app/tonconnect-manifest.json',
 })
